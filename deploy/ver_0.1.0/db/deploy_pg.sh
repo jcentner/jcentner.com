8e8509a1fc7f4e09af37ade9e03bc148aa91412a
Jake Centner <31484717+jcentner@users.noreply.github.com>
2023-03-24 13:56:04 -0600

    wip deploy_pg

--- deploy/ver_0.1.0/db/deploy_pg.sh
+++ deploy/ver_0.1.0/db/deploy_pg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 packages=('postgresql' 'postgresql-contrib')
+self='jcentner' # my username - automate this from who ran file, maybe?
 username='pgserver'
 database='pgserver'
+pg_hba='/etc/postgresql/14/main/pg_hba.conf'
 logfile='pg-install.log'
 
 echo "Updating server..."
@@ -20,13 +22,18 @@ echo "Gathering deployment scripts..."
 shopt -s nullglob
 initdb=(./*.sql)
 
-echo "Creating roles and database..."
+echo "Creating user, role, and database..."
+# for added security, make username/database a newuser (and sudo useradd newuser)
+# then define and use a password for newuser with an environment variable
 sudo -u postgres createuser -s $username >> $logfile
 sudo -u postgres createdb $username >> $logfile
+sudo echo "local $database $self trust" >> $pg_hba # for added security, use peer only for newuser
+sudo pg_ctl reload
+sleep 3
 
 for file in ${initdb[@]}; do
 	echo "Running $file..."
-	psql -U $username -d $database -a -o $logfile -f file # add -q for quiet if logfile gets welcome messages, etc. 
+	su $self -c "cd psql -U $username -d $database -a -f $file" # add -q for quiet if logfile gets welcome messages, etc.
 done
 
 echo "Deployment done; $database database created with user $username"
