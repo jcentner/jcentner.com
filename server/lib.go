929e1d9080ac2d03b8d9fadf5c468279eaad5a23
Jake Centner <31484717+jcentner@users.noreply.github.com>
2023-04-04 14:08:08 -0600

    lib rename globals

--- server/lib.go
+++ server/lib.go
@@ -15,8 +15,8 @@ import (
 )
 
 // ReadJSON: read JSON and unmarshal into a go struct
+
 func ReadJSON(filename string, gostruct interface{}) (err error) {
-	var buffer []byte
 	buffer, err = ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Printf("ReadJSON encountered error: %s\n", err)
@@ -28,17 +28,22 @@ func ReadJSON(filename string, gostruct interface{}) (err error) {
 
 // PGXConnect populates "conn" global that can connect to a postgres instance
 // Requires DATABASE_URL env var set
+// Requires "conn" and "ctx" globals
+// var conn *pgxpool.Pool
+// var ctx context.Context
+
 func PGXConnect() {
-	context = context.Background()
+	ctx = context.Background()
 	conurl := os.Getenv("DATABASE_URL")
 
-	if conn, err := pgxpool.Connect(context, conurl); err != nil {
+	if conn, err := pgxpool.Connect(ctx, conurl); err != nil {
 		fmt.Printf("Unable to connect to %s; error: %v\n", conurl, err)
 		os.Exit(1)
 	}
 }
 
 //PGXDisconnect closes "conn" global connection
+
 func PGXDisconnect() {
 	conn.Close()
 }
