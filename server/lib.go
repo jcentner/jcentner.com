8a71f129fb103e51c9aabd8fcc3fe73d660d2635
Jake Centner <31484717+jcentner@users.noreply.github.com>
2023-04-04 14:06:15 -0600

    first pass lib for some resuse; db funcs

---
+++ server/lib.go
@@ -0,0 +1,44 @@
+package main
+
+// Copyright (C) Jacob Centner, 2023
+// BSD 3 Clause Licensed
+// See /LICENSE.bsd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+// ReadJSON: read JSON and unmarshal into a go struct
+func ReadJSON(filename string, gostruct interface{}) (err error) {
+	var buffer []byte
+	buffer, err = ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("ReadJSON encountered error: %s\n", err)
+		return
+	}
+	err = json.Unmarshal(buffer, gostruct)
+	return
+}
+
+// PGXConnect populates "conn" global that can connect to a postgres instance
+// Requires DATABASE_URL env var set
+func PGXConnect() {
+	context = context.Background()
+	conurl := os.Getenv("DATABASE_URL")
+
+	if conn, err := pgxpool.Connect(context, conurl); err != nil {
+		fmt.Printf("Unable to connect to %s; error: %v\n", conurl, err)
+		os.Exit(1)
+	}
+}
+
+//PGXDisconnect closes "conn" global connection
+func PGXDisconnect() {
+	conn.Close()
+}
