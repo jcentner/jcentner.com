2b8e726315329a19eef48308e55f71ece1261fdc
Jake Centner <31484717+jcentner@users.noreply.github.com>
2023-04-17 14:39:21 -0600

    fix js for navbar darkening

--- html/js/site.js
+++ html/js/site.js
@@ -1,10 +1,17 @@
-// darken navbar when at top
+// darken navbar when at top or when mobile menu shows
 const nav = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
 
 window.onscroll = function() {
+
+	toggleNavbarDarken();
+}
+
+function toggleNavbarDarken() {
 	const top = window.scrollY;
 	
-	if (top >= 100) {
+	if (top >= 100 || menuToggle.classList.contains('show')) {
 		nav.classList.add('navbarDarken');
 	}
 	else {
@@ -12,10 +19,15 @@ window.onscroll = function() {
 	}
 }
 
-// collapse navbar after clicked on mobile
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
+menuToggle.addEventListener('show.bs.collapse', () => {
+	nav.classList.add('navbarDarken');
+});
 
+menuToggle.addEventListener('hidden.bs.collapse', () => {
+	toggleNavbarDarken();
+});
+
+// collapse navbar after clicked on mobile
 navLinks.forEach((link) => {
-	link.addEventListener('click', () => { new boostrap.Collapse(menuToggle).toggle() })
+	link.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
